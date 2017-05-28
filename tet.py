109eaf4a4160aba04a98bde4d0351c31ef6650d9
karice <kl529@naver.com>
2017-05-28 20:20:58 +0900

    make button quit

---
+++ tet.py
@@ -0,0 +1,29 @@
+import sys , os , math , random
+import pygame
+from pygame. locals import *
+
+def main():
+	intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        gameDisplay.fill(white)
+        largeText = pygame.font.Font('freesansbold.ttf',115)
+        TextSurf, TextRect = text_objects("A bit Racey", largeText)
+        TextRect.center = ((display_width/2),(display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+
+        pygame.draw.rect(gameDisplay, green,(150,450,100,50))
+        pygame.draw.rect(gameDisplay, red,(550,450,100,50))
+
+
+        pygame.display.update()
+        clock.tick(15)
+
+if __name__ == '__min__':
+	main()
